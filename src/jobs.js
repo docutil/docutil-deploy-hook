b83978031c4cb201f9553fe4c984298439b72c18
yuekcc <yuekcc@qq.com>
2024-10-10 22:53:59 +0800

    输出 .mjs 文件
    构建输出改为 esm 模块了

--- src/jobs.js
+++ src/jobs.js
@@ -8,16 +8,16 @@ class DeployJobQueue {
    * @param {() => Promise<void>} createJob
    * @param {string} id
    */
-  add(createJob, id) {
+  add(doDeploy, id) {
     console.log('[JOBS] add job');
 
     if (!this._jobs.has(id)) {
       console.log('[JOBS] createJob with id =', id);
-      const wrappedJOb = createJob().finally(() => {
+      const wrappedJob = doDeploy().finally(() => {
         this._jobs.delete(id);
       });
 
-      this._jobs.set(id, wrappedJOb);
+      this._jobs.set(id, wrappedJob);
     } else {
       console.log('[JOBS] exists same job indexed by id, id =', id);
     }
